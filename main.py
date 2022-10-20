2b8df88db89869937334ef00ae8bf9465fab5f9b
Sarik <115519782+Sarikinho@users.noreply.github.com>
2022-10-20 00:21:28 +0000

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+# Meu primeiro projeto Python
+# Isso é uma linha de comentario feita na linguagem português
+'''
+Ih rapaz, Aqui temos um comentário em bloco.
+'''
+
+{
+  print("Hello world")
+
+}
